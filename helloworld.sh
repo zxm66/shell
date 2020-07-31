ae034cd2c741f6c2dbd54ba632a60068e39026f9
zxm66 <1396729865@qq.com>
2020-07-31 15:42:08 +0800

    gc

---
+++ helloworld.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+# -*- coding: utf-8 -*- 
+ 
+######################################################################
+# 
+# Copyright (c) 2017 kinva.cc, Inc. All Rights Reserved
+# 
+# @author zhangxiaomin(1396729865@qq.com)
+# 
+######################################################################
+ 
+
+
+echo "hello world"
+echo "how are you ?"
+
+NAME="zhangxiaomin"
+AGE=18
+
+echo "name : $NAME"
+echo "age : $AGE"
+
+
+
